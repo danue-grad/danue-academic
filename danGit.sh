81fdc57c4760dc78030374937e8d32a001975f48
Dan Uehara <danue@uio.no>
2022-02-28 16:17:26 +0100

    tests5

--- danGit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-{ #Try
-git add * &&
-(git commit -m $1 || (git rm --cached * && false )) && 
-(git push || (git reset HEAD~ && false ))
-} || { #Catch
-  echo "Something went wrong!"
-}
